ffa0195421f0bd3fe09c1d63586d4340917c869b
Alexander Almeida-Ramírez <a.almeidar@uniandes.edu.co>
2024-02-07 20:58:09 -0500

    Update code.R

--- aalmeira/code.R
+++ aalmeira/code.R
@@ -2,3 +2,5 @@ iris2 <- read.csv("~/GitHub/arepa_de_prueba/input/iris.csv")
 install.packages("haven")
 library(haven)
 write_dta(iris2, "~/GitHub/arepa_de_prueba/input/iris.dta")
+
+write_dta(iris2, "~/GitHub/arepa_de_prueba/input/iris_v2.dta")
